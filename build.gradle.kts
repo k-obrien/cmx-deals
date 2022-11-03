c532ff0639a942e883fec90ff7543044a4f231fa
Kieran O'Brien <kieran@obrien.id>
2022-11-03 21:31:29 +1100

    Init repo

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+  kotlin("multiplatform") version "1.7.21"
+}
+
+group = "network.obrien"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+kotlin {
+  val nativeTarget = when (System.getProperty("os.name")) {
+    "Mac OS X" -> when (System.getProperty("os.arch")) {
+      in listOf("aarch64", "arm-v8", "arm64") -> macosArm64("native")
+      in listOf("x86-64", "x86_64", "amd64", "x64") -> macosX64("native")
+      else -> throw GradleException("Host architecture is not supported.")
+    }
+    "Linux" -> linuxX64("native")
+    else -> throw GradleException("Host OS is not supported.")
+  }
+
+  nativeTarget.apply {
+    binaries {
+      executable {
+        entryPoint = "main"
+      }
+    }
+  }
+
+  sourceSets {
+    val nativeMain by getting
+    val nativeTest by getting
+  }
+}
